e7a71525dfa3c6af213c8eeea8693ee0151b93a5
Vinicius Crestani <vinicr@gmail.com>
2021-08-07 18:34:14 -0300

    init

---
+++ src/app/core/user/user.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { TokenService } from '../token/token.service';
+import { BehaviorSubject } from 'rxjs';
+import { User } from './user';
+import * as jtw_decode from 'jwt-decode';
+
+@Injectable({ providedIn: 'root'})
+export class UserService { 
+
+    private userSubject = new BehaviorSubject<User>(null);
+    private userName: string;
+
+    constructor(private tokenService: TokenService) { 
+
+        this.tokenService.hasToken() && 
+            this.decodeAndNotify();
+    }
+
+    setToken(token: string) {
+        this.tokenService.setToken(token);
+        this.decodeAndNotify();
+    }
+
+    getUser() {
+        return this.userSubject.asObservable();
+    }
+
+    private decodeAndNotify() {
+        const token = this.tokenService.getToken();
+        const user = jtw_decode(token) as User;
+        const user2 = jtw_decode(token);
+        console.log(user, typeof(user));
+        console.log(user2, typeof(user2));
+        this.userName = user.name;
+        this.userSubject.next(user);
+    }
+
+    logout() {
+        this.tokenService.removeToken();
+        this.userSubject.next(null);
+    }
+
+    isLogged() {
+        return this.tokenService.hasToken();
+    }
+
+    getUserName() {
+        return this.userName;
+    }
+}
